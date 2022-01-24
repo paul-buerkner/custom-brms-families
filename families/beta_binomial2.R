7dcde636fe5ca08aaaee54040f0ea71027e58e03
Maximilian Scholz <m0hawk@gmx.de>
2022-01-24 17:00:05 +0100

    Update beta_binomial family

--- families/beta_binomial2.R
+++ families/beta_binomial2.R
@@ -5,39 +5,40 @@
 library(brms)
 
 # helper functions for post-processing of the family
-log_lik_beta_binomial2 <- function(i, draws) {
-  mu <- draws$dpars$mu[, i]
-  phi <- draws$dpars$phi
-  N <- draws$data$trials[i]
-  y <- draws$data$Y[i]
-  beta_binomial2_lpmf(y, mu, phi, N)
+log_lik_beta_binomial2 <- function(i, prep) {
+  mu <- brms::get_dpar(prep, "mu", i = i)
+  phi <- brms::get_dpar(prep, "phi", i = i)
+  trials <- prep$data$vint1[i]
+  y <- prep$data$Y[i]
+  beta_binomial2_lpmf(y, mu, phi, trials)
 }
 
-predict_beta_binomial2 <- function(i, draws, ...) {
-  mu <- draws$dpars$mu[, i]
-  phi <- draws$dpars$phi
-  N <- draws$data$trials[i]
-  beta_binomial2_rng(mu, phi, N)
+posterior_predict_beta_binomial2 <- function(i, prep, ...) {
+  mu <- brms::get_dpar(prep, "mu", i = i)
+  phi <- brms::get_dpar(prep, "phi", i = i)
+  trials <- prep$data$vint1[i]
+  beta_binomial2_rng(mu, phi, trials)
 }
 
-fitted_beta_binomial2 <- function(draws) {
-  mu <- draws$dpars$mu
-  trials <- draws$data$trials
+posterior_epred_beta_binomial2 <- function(prep) {
+  mu <- brms::get_dpar(prep, "mu")
+  trials <- prep$data$vint1
   trials <- matrix(trials, nrow = nrow(mu), ncol = ncol(mu), byrow = TRUE)
   mu * trials
 }
 
 # definition of the custom family
 beta_binomial2 <- custom_family(
-  "beta_binomial2", 
+  "beta_binomial2",
   dpars = c("mu", "phi"),
-  links = c("logit", "log"), 
-  lb = c(0, 0), 
+  links = c("logit", "log"),
+  lb = c(0, 0),
   ub = c(1, NA),
-  type = "int", vars = "trials[n]",
+  type = "int",
+   vars = "trials[n]",
   log_lik = log_lik_beta_binomial2,
-  predict = predict_beta_binomial2,
-  fitted = fitted_beta_binomial2
+  posterior_predict = posterior_predict_beta_binomial2,
+  posterior_epred = posterior_epred__beta_binomial2
 )
 
 # additionally required Stan code
