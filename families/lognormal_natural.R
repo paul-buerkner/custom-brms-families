a4ce423449d61054730d03bbcfc002124f9cc697
Staffan Betnér <staffan@betner.nu>
2022-03-03 10:33:18 +0100

    add one loo, fix of vectorization of base R functions
    Noted that the example did not work, so I fixed that.

--- families/lognormal_natural.R
+++ families/lognormal_natural.R
@@ -8,7 +8,7 @@ log_lik_lognormal_natural <- function(i, prep) {
   if(NCOL(prep$dpars$sigma)==1){sigma <- prep$dpars$sigma}else
   {sigma <- prep$dpars$sigma[, i]}   ## [, i] if sigma is modelled, without otherwise
   y <- prep$data$Y[i]
-  common_term = log(1+sigma^2/mu^2)
+  common_term = log(1+(sigma/mu)^2)
   Vectorize(dlnorm)(y, log(mu)-common_term/2, sqrt(common_term), log = TRUE)
 }
 
@@ -17,8 +17,8 @@ posterior_predict_lognormal_natural <- function(i, prep, ...) {
   mu <- prep$dpars$mu[, i]
   if(NCOL(prep$dpars$sigma)==1){sigma <- prep$dpars$sigma}else
   {sigma <- prep$dpars$sigma[, i]}   ## [, i] if sigma is modelled, without otherwise
-  common_term = log(1+sigma^2/mu^2)
-  rlnorm(n, log(mu)-common_term/2, sqrt(common_term))
+  common_term = log(1+(sigma/mu)^2)
+  Vectorize(rlnorm)(1, log(mu)-common_term/2, sqrt(common_term))
 }
 
 posterior_epred_lognormal_natural <- function(prep) {
@@ -37,12 +37,12 @@ custom_family(name = "lognormal_natural",
 # additionally required Stan code
 stan_lognormal_natural <- "
   real lognormal_natural_lpdf(real y, real mu, real sigma) {
-    real common_term = log(1+sigma^2/mu^2);
+    real common_term = log(1+(sigma/mu)^2);
     return lognormal_lpdf(y | log(mu)-common_term/2, 
                               sqrt(common_term));
   }
   real lognormal_natural_rng(real mu, real sigma) {
-    real common_term = log(1+sigma^2/mu^2);
+    real common_term = log(1+(sigma/mu)^2);
     return lognormal_rng(log(mu)-common_term/2, 
                             sqrt(common_term));
   }
@@ -63,5 +63,7 @@ model_test2 |> conditional_effects()
 
 model_test1 |> conditional_effects(method = "posterior_predict")
 model_test2 |> conditional_effects(method = "posterior_predict")
+
+loo(model_test1)
 
 loo(model_test1, model_test2)
